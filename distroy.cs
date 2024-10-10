2c3ac96b8c3a0343f5304e4e24585a0e8a606457
DApl5 <danplay2010@gmail.com>
2024-10-10 16:44:32 +0300

    Add files via upload
    distroy

---
+++ distroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject);
+        //this is
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
